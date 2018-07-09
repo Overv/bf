a654bbaa256d8a18e3bc3521016d42c17714ec3e
Alexander Overvoorde <overv161@gmail.com>
2018-07-10 00:32:50 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,9 @@
+# bf
+
+This is a basic Brainfuck interpreter I wrote to practice Rust programming.
+
+## Usage
+
+Example:
+
+    cargo run --release samples/mandelbrot.bf
---
+++ src/main.rs
@@ -0,0 +1,156 @@
+use std::env;
+use std::io::Read;
+use std::fs::File;
+
+/// Opcodes determined by the lexer
+#[derive(Debug)]
+#[derive(Clone)]
+enum OpCode {
+    IncrementPointer,
+    DecrementPointer,
+    Increment,
+    Decrement,
+    Write,
+    Read,
+    LoopBegin,
+    LoopEnd,
+}
+
+#[derive(Debug)]
+#[derive(Clone)]
+enum Instruction {
+    IncrementPointer,
+    DecrementPointer,
+    Increment,
+    Decrement,
+    Write,
+    Read,
+    Loop(Vec<Instruction>)
+}
+
+/// Lexer turns the source code into a sequence of opcodes
+fn lex(source: String) -> Vec<OpCode> {
+    let mut operations = Vec::new();
+
+    for symbol in source.chars() {
+        let op = match symbol {
+            '>' => Some(OpCode::IncrementPointer),
+            '<' => Some(OpCode::DecrementPointer),
+            '+' => Some(OpCode::Increment),
+            '-' => Some(OpCode::Decrement),
+            '.' => Some(OpCode::Write),
+            ',' => Some(OpCode::Read),
+            '[' => Some(OpCode::LoopBegin),
+            ']' => Some(OpCode::LoopEnd),
+            _ => None
+        };
+
+        // Non-opcode characters are simply comments
+        match op {
+            Some(op) => operations.push(op),
+            None => ()
+        }
+    }
+
+    operations
+}
+
+fn parse(opcodes: Vec<OpCode>) -> Vec<Instruction> {
+    let mut program: Vec<Instruction> = Vec::new();
+    let mut loop_stack = 0;
+    let mut loop_start = 0;
+
+    for (i, op) in opcodes.iter().enumerate() {
+        if loop_stack == 0 {
+            let instr = match op {
+                OpCode::IncrementPointer => Some(Instruction::IncrementPointer),
+                OpCode::DecrementPointer => Some(Instruction::DecrementPointer),
+                OpCode::Increment => Some(Instruction::Increment),
+                OpCode::Decrement => Some(Instruction::Decrement),
+                OpCode::Write => Some(Instruction::Write),
+                OpCode::Read => Some(Instruction::Read),
+
+                OpCode::LoopBegin => {
+                    loop_start = i;
+                    loop_stack += 1;
+                    None
+                },
+
+                OpCode::LoopEnd => panic!("unmatched loop ending"),
+            };
+
+            match instr {
+                Some(instr) => program.push(instr),
+                None => ()
+            }
+        } else {
+            match op {
+                OpCode::LoopBegin => {
+                    loop_stack += 1;
+                },
+                OpCode::LoopEnd => {
+                    loop_stack -= 1;
+
+                    if loop_stack == 0 {
+                        program.push(Instruction::Loop(parse(opcodes[loop_start+1..i].to_vec())));
+                    }
+                },
+                _ => (),
+            }
+        }
+    }
+
+    program
+}
+
+/// Executes a program that was previously parsed
+fn run(instructions: &Vec<Instruction>, tape: &mut Vec<u8>, data_pointer: &mut usize) {
+    for instr in instructions {
+        match instr {
+            Instruction::IncrementPointer => *data_pointer += 1,
+            Instruction::DecrementPointer => *data_pointer -= 1,
+            Instruction::Increment => tape[*data_pointer] += 1,
+            Instruction::Decrement => tape[*data_pointer] -= 1,
+            Instruction::Write => print!("{}", tape[*data_pointer] as char),
+            Instruction::Read => {
+                let mut input: [u8; 1] = [0; 1];
+                std::io::stdin().read_exact(&mut input).expect("failed to read stdin");
+                tape[*data_pointer] = input[0];
+            },
+            Instruction::Loop(nested_instructions) => {
+                while tape[*data_pointer] != 0 {
+                    run(&nested_instructions, tape, data_pointer)
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    // Determine which file to execute
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("usage: bf <file.bf>");
+        std::process::exit(1);
+    }
+    
+    let filename = &args[1];
+
+    // Read file
+    let mut file = File::open(filename).expect("program file not found");
+    let mut source = String::new();
+    file.read_to_string(&mut source).expect("failed to read program file");
+
+    // Lex file into opcodes
+    let opcodes = lex(source);
+
+    // Parse opcodes into program
+    let program = parse(opcodes);
+
+    // Set up environment and run program
+    let mut tape: Vec<u8> = vec![0; 1024];
+    let mut data_pointer = 512;
+
+    run(&program, &mut tape, &mut data_pointer);
+}
